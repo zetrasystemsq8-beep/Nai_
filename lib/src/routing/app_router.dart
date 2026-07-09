ac155c218899b1f553478c8d2ab15b83fc691410
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-09 11:57:02 +0100

    Update app_router.dart

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nai/src/routing/global_navigator.dart';
