19f055c6e9cdbd18b4eb7cc79493d36b8489cf3c
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-10 08:42:49 +0100

    Update challenge_provider.dart

--- lib/src/features/challenges/presentation/providers/challenge_provider.dart
+++ lib/src/features/challenges/presentation/providers/challenge_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../data/challenge_generator.dart';
 import '../../data/coin_wallet_store.dart';
+import '../../data/game_progress_store.dart';
 
 final challengeGeneratorProvider = Provider((ref) => ChallengeGenerator());
 final coinWalletProvider = Provider((ref) => CoinWalletStore());
+final gameProgressProvider = Provider((ref) => GameProgressStore());
