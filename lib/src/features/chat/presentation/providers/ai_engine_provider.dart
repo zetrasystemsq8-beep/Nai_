138020ec9f01c4094025e27b0d7b7f9d4b33b91f
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-11 10:13:14 +0100

    Update ai_engine_provider.dart

--- lib/src/features/chat/presentation/providers/ai_engine_provider.dart
+++ lib/src/features/chat/presentation/providers/ai_engine_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nai/src/features/nigeria/presentation/providers/news_provider.dart';
 import 'package:nai/src/features/nigeria/presentation/providers/wiki_provider.dart';
 
 import '../../domain/ai_engine.dart';
@@ -7,7 +6,6 @@ import '../../data/grounded_ai_engine.dart';
 
 final aiEngineProvider = Provider<AIEngine>((ref) {
   return GroundedAIEngine(
-    newsRepository: ref.watch(newsRepositoryProvider),
     wikiRepository: ref.watch(wikiRepositoryProvider),
   );
 });
