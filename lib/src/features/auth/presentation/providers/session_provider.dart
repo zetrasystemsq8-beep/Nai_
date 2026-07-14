250830f158540729f962f0ddaf4f3622259fe878
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-14 13:44:11 +0100

    Update session_provider.dart

--- lib/src/features/auth/presentation/providers/session_provider.dart
+++ lib/src/features/auth/presentation/providers/session_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:nai/src/features/auth/domain/entities/user.dart';
 import 'package:nai/src/features/auth/domain/repositories/auth_repository.dart';
 import 'package:nai/src/features/auth/data/repositories/auth_repository_impl.dart';
