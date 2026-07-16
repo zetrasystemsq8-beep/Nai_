b71f697ba74b84eb6cbdb4c3f745e40c0268165a
ihatesadiqt-dev <ihatesadiqt@gmail.com>
2026-07-16 11:54:55 +0100

    refactor: add zetraId and zetraMail fields to AppUser model

--- lib/src/features/auth/data/models/user_model.dart
+++ lib/src/features/auth/data/models/user_model.dart
@@ -5,12 +5,16 @@ class AppUser extends Equatable {
   final String email;
   final String? name;
   final String? photoUrl;
+  final String? zetraId;
+  final String? zetraMail;
 
   const AppUser({
     required this.id,
     required this.email,
     this.name,
     this.photoUrl,
+    this.zetraId,
+    this.zetraMail,
   });
 
   factory AppUser.empty() => const AppUser(id: '', email: '');
@@ -19,5 +23,5 @@ class AppUser extends Equatable {
   bool get isNotEmpty => id.isNotEmpty;
 
   @override
-  List<Object?> get props => [id, email, name, photoUrl];
+  List<Object?> get props => [id, email, name, photoUrl, zetraId, zetraMail];
 }
